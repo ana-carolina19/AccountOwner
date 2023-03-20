3cbe57a8a81c7b51f1eda861919458cdf3d30ab7
Ana Carolina Antonio de Sousa <carolisousa102016@gmail.com>
2023-03-20 20:38:48 -0300

    20.03
    começando o front-end

---
+++ frontend/AccountOwnerClient/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CollapseModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
